f796b3e0b3303029b589826162f4802f40c0385f
Jess Frazelle <github@jessfraz.com>
2023-07-04 15:21:32 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-//! The twenty-twenty library allows for visual regression testing of H.264 frames and images.
+//! The `twenty-twenty` library allows for visual regression testing of H.264 frames and images.
 //! It makes it easy to update the contents when should be updated to match the new results.
 //!
-//! Use it like this:
+//! Each function takes a score threshold, which is the lowest possible "score" you are willing for
+//! the image comparison to return. If the resulting score is less than than the threshold, the test
+//! will fail. The score must be a number between 0 and 1. If the images are the exact same, the
+//! score will be 1.
+//!
+//! Use it like this for an H.264 frame:
 //!
 //! ```no_run
 //! # fn get_frame() -> (u32, u32, Vec<u8>) {
@@ -9,13 +14,23 @@
 //! # }
 //!
 //! let (width, height, actual) = get_frame();
-//! twenty_twenty::assert_h264_frame("frame_image.png", width, height, &actual, 1.0);
+//! twenty_twenty::assert_h264_frame("frame_image.png", width, height, &actual, 0.9);
+//! ```
+//! Use it like this for an image:
+//!
+//! ```no_run
+//! # fn get_image() -> image::DynamicImage {
+//! #    todo!()
+//! # }
+//!
+//! let actual = get_image();
+//! twenty_twenty::assert_image("og_image.png", &actual, 0.9);
 //! ```
 //!
-//! If the output doesn't match, the program will panic! and emit the
+//! If the output doesn't match, the program will `panic!` and emit the
 //! difference in the score.
 //!
-//! To accept the changes from `get_frame()`, run with `TWENTY_TWENTY=overwrite`.
+//! To accept the changes from `get_frame()` or `get_image()`, run with `TWENTY_TWENTY=overwrite`.
 
 #![deny(missing_docs)]
 
