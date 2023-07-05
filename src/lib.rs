a3a5a4a5112620a81c145aadfa3062973caff04d
Jess Frazelle <jessfraz@users.noreply.github.com>
2023-07-05 12:24:47 -0700

    ffmpeg impl (#2)
    * testing
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * ffmeg impl
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * install ffmpeg
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * better docs
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * bump the version
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fix tests
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * better samples
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * better install
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * updates
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * less deps
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fixes
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fixes
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    * fixes
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>
    
    ---------
    
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- src/lib.rs
+++ src/lib.rs
@@ -10,31 +10,38 @@
 //! quality degradation that is caused by processing such as data compression or by losses in data
 //! transmission. More information can be found [here](https://en.wikipedia.org/wiki/Structural_similarity).
 //!
+//! You will need `ffmpeg` installed on your system to use this library. This library uses
+//! the [ffmpeg bindings](https://docs.rs/ffmpeg-next/latest/ffmpeg_next/) in rust to convert the H.264 frames to images.
+//!
 //! Use it like this for an H.264 frame:
 //!
-//! ```no_run
-//! # fn get_frame() -> (u32, u32, Vec<u8>) {
-//! #     (0, 0, vec![])
+//! ```rust
+//! # fn get_h264_frame() -> Vec<u8> {
+//! #     std::fs::read("tests/initial-grid.h264").unwrap()
 //! # }
-//! let (width, height, actual) = get_frame();
-//! twenty_twenty::assert_h264_frame("frame_image.png", width, height, &actual, 0.9);
+//! let actual = get_h264_frame();
+//! twenty_twenty::assert_h264_frame("tests/initial-grid.png", &actual, 0.9);
 //! ```
 //! Use it like this for an image:
 //!
-//! ```no_run
+//! ```rust
 //! # fn get_image() -> image::DynamicImage {
-//! #    todo!()
+//! #    image::io::Reader::open("tests/dog1.png").unwrap().decode().unwrap()
 //! # }
 //! let actual = get_image();
-//! twenty_twenty::assert_image("og_image.png", &actual, 0.9);
+//! twenty_twenty::assert_image("tests/dog1.png", &actual, 0.9);
 //! ```
 //!
 //! If the output doesn't match, the program will `panic!` and emit the
 //! difference in the score.
 //!
-//! To accept the changes from `get_frame()` or `get_image()`, run with `TWENTY_TWENTY=overwrite`.
+//! To accept the changes from `get_h264_frame()` or `get_image()`, run with `TWENTY_TWENTY=overwrite`.
 
 #![deny(missing_docs)]
+
+use std::io::Write;
+
+extern crate ffmpeg_next as ffmpeg;
 
 use anyhow::Result;
 
@@ -62,8 +69,8 @@ pub fn assert_image<P: AsRef<std::path::Path>>(path: P, actual: &image::DynamicI
 /// This compartes the H.264 frame to a PNG. This is because then the diff will be easily visible
 /// in a UI like GitHub's.
 #[track_caller]
-pub fn assert_h264_frame<P: AsRef<std::path::Path>>(path: P, width: u32, height: u32, actual: &[u8], threshold: f64) {
-    match h264_frame_to_image(width, height, actual) {
+pub fn assert_h264_frame<P: AsRef<std::path::Path>>(path: P, actual: &[u8], threshold: f64) {
+    match h264_frame_to_image(actual) {
         Ok(image) => {
             if let Err(e) = assert_image_impl(path, &image, threshold) {
                 panic!("assertion failed: {e}")
@@ -76,12 +83,59 @@ pub fn assert_h264_frame<P: AsRef<std::path::Path>>(path: P, width: u32, height:
 }
 
 // Convert a H264 frame to an image.
-pub(crate) fn h264_frame_to_image(width: u32, height: u32, data: &[u8]) -> Result<image::DynamicImage> {
-    let Some(raw) = image::RgbaImage::from_raw(width, height, data.to_vec()) else {
+pub(crate) fn h264_frame_to_image(data: &[u8]) -> Result<image::DynamicImage> {
+    // Initialize the FFmpeg library
+    ffmpeg::init()?;
+
+    // Save the frame to a temporary file, we can read back out of.
+    // This will automatically be deleted when the program exits.
+    // TODO: this sucks we have to write this back out to disk, we should find a better way
+    // to create a decoder from just bytes.
+    let temp_file_name = std::env::temp_dir().join(format!("{}.h264", uuid::Uuid::new_v4()));
+    let mut temp_file = std::fs::File::create(&temp_file_name)?;
+    temp_file.write_all(data)?;
+
+    // Create a decoder for the H.264 format
+    let ictx = ffmpeg::format::input(&temp_file_name).map_err(|e| anyhow::anyhow!(e))?;
+    let input = ictx
+        .streams()
+        .best(ffmpeg::media::Type::Video)
+        .ok_or(ffmpeg::Error::StreamNotFound)?;
+    let context = ffmpeg::codec::context::Context::from_parameters(input.parameters())?;
+    let mut video_decoder = context.decoder().video()?;
+
+    // Read the H.264 frame
+    let mut video_frame = ffmpeg::frame::Video::empty();
+    let packet = ffmpeg::packet::Packet::copy(data);
+
+    // Decode the H.264 frame
+    video_decoder.send_packet(&packet)?;
+    video_decoder.receive_frame(&mut video_frame)?;
+    video_decoder.flush();
+
+    // Get the pixel format of the decoded frame
+    let pixel_format = video_frame.format();
+    println!("pixel format: {:?}", pixel_format);
+    if pixel_format != ffmpeg::format::Pixel::RGB24 {
+        let mut converted_video = ffmpeg::frame::Video::empty();
+        // Convert the decoded frame to an RGB format.
+        video_frame
+            .converter(ffmpeg::format::Pixel::RGB24)?
+            .run(&video_frame, &mut converted_video)?;
+        video_frame = converted_video;
+    }
+
+    // Convert the decoded frame to an RGB format
+    video_frame.set_format(ffmpeg::format::Pixel::RGB24);
+
+    // Save the image to disk.
+
+    // Create an image from the RGB frame
+    let Some(raw) = image::RgbImage::from_raw(video_frame.width(), video_frame.height(), video_frame.data(0).to_vec()) else {
         anyhow::bail!("could not parse image from raw");
     };
 
-    Ok(image::DynamicImage::ImageRgba8(raw))
+    Ok(image::DynamicImage::ImageRgb8(raw))
 }
 
 pub(crate) fn assert_image_impl<P: AsRef<std::path::Path>>(
