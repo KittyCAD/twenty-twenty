7bd36f937914783440172042b21b9fe421599e02
Adam Chalmers <adam.chalmers@zoo.dev>
2025-11-10 13:34:17 -0600

    Add a new 'Update' mode (#48)
    * Add a new 'Update' mode
    
    TWENTY_TWENTY=overwrite will ALWAYS overwrite the image,
    even if the results are within the threshold. This makes
    PRs full of noisy file changes that are totally within
    the threshold and didn't need to be updated.
    
    The new TWENTY_TWENTY=update mode will only write
    the image to disk if the results were beyond the
    threshold. This way, PRs only show relevant changes
    that need inspecting (i.e. were beyond the threshold)
    
    * Rename variant to UpdateOnMismatch

--- src/lib.rs
+++ src/lib.rs
@@ -92,6 +92,8 @@ enum Mode {
     Default,
     /// Overwrite the file we are comparing against, i.e. accept the changes of the diff.
     Overwrite,
+    /// Overwrite the file if they don't match, but leave it alone if they do.
+    UpdateOnMismatch,
     /// Store the files on disk always (for now make all paths relative to `artifacts/`).
     StoreArtifact,
     /// Store the files on disk when they don't match (for now make all paths relative to `artifacts/`).
@@ -104,6 +106,7 @@ impl std::str::FromStr for Mode {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         Ok(match s {
             "overwrite" => Mode::Overwrite,
+            "update" => Mode::UpdateOnMismatch,
             "store-artifact" => Mode::StoreArtifact,
             "store-artifact-on-mismatch" => Mode::StoreArtifactOnMismatch,
             _ => Mode::Default,
@@ -181,6 +184,12 @@ pub(crate) fn assert_image_impl<P: AsRef<std::path::Path>>(
     }
 
     if image_mismatch {
+        if mode == Mode::UpdateOnMismatch {
+            if let Err(e) = actual.save_with_format(path, image::ImageFormat::Png) {
+                panic!("unable to write image to {}: {}", path.display(), e);
+            }
+            return Ok(());
+        }
         anyhow::bail!(
             r#"image (`{}`) score is `{}` which is less than min_permissible_similarity `{}`
                 set {}=overwrite if these changes are intentional"#,
